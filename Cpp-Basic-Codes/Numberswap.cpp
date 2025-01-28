61c634c282f15a4740326dd0ed48584fe789be5d
KareemHassan <kareem99710@gmail.com>
2025-01-28 18:07:19 +0200

    2NumberSwap

---
+++ Cpp-Basic-Codes/Numberswap.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+void way1 (int a, int b){
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    cout << "a: " << a << " b: " << b << endl;
+}
+void way2 (int a, int b){
+    a = a * b;
+    b = a / b;
+    a = a / b;
+    cout << "a: " << a << " b: " << b << endl;
+}
+void way3 (int a, int b){
+    int c = a;
+    a = b;
+    b = c;
+    cout << "a: " << a << " b: " << b << endl;
+}
+
+int main{
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    way1(a, b);
+    way2(a, b);
+    way3(a, b);
+    return 0;
+}
