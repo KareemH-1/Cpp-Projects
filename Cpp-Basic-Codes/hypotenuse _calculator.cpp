e72f211e13e35d54c6ef15d305f1d48b0b2afa9b
KareemHassan <kareem99710@gmail.com>
2025-01-30 21:13:32 +0200

    output and under_root variables changed to floats instead of int , also added main function

--- Cpp-Basic-Codes/hypotenuse _calculator.cpp
+++ Cpp-Basic-Codes/hypotenuse _calculator.cpp
@@ -3,8 +3,17 @@
 using namespace std;
 
 float hypotenuse(float base, float perpendicular) {
-   int output;
-   int under_root = (base * base) + (perpendicular * perpendicular);
+   float output;
+   float under_root = (base * base) + (perpendicular * perpendicular);
     output = sqrt(under_root);
     return (output);
+}
+
+int main() {
+    float base, perpendicular;
+    cout << "Enter the base of the triangle: ";
+    cin >> base;
+    cout << "Enter the perpendicular of the triangle: ";
+    cin >> perpendicular;
+    cout << "Hypotenuse of the triangle is: " << hypotenuse(base, perpendicular);
 }
