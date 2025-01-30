5a296ae847e55525bbe3b2c323e822c594205555
KareemHassan <kareem99710@gmail.com>
2025-01-30 20:49:02 +0200

    put the algorithm inside a function

--- Cpp-Basic-Codes/Sum_DigitsOfA_Number.cpp
+++ Cpp-Basic-Codes/Sum_DigitsOfA_Number.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
+int sumOfDigits(int n) {
     int sum = 0;
-        for(;n > 0;) {
+    for(;n > 0;) {
         sum += n % 10;
         n /= 10;
     }
-    cout << "Sum of digits: " << sum << endl;
+    return (sum);
+}
+
+int main() {
+    int n;
+     cout << "Enter a number: ";
+    cin >> n;
+    cout << "Sum of digits of the number " << n << " is " << sumOfDigits(n) << endl;
 }
