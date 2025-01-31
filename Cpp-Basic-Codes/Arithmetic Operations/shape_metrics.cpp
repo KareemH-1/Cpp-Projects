e7b26f011d8de8a7234072d62c699388b5c553ea
KareemHassan <kareem99710@gmail.com>
2025-01-31 23:53:06 +0200

    Right-angled triangle added

--- Cpp-Basic-Codes/Arithmetic Operations/shape_metrics.cpp
+++ Cpp-Basic-Codes/Arithmetic Operations/shape_metrics.cpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 
 // Function for square
@@ -17,6 +18,15 @@ void square(int L, int &Area, int &Per) {
 void rectangle(int L, int W, int &Area, int &Per) {
     Area = L * W;
     Per = 2 * (L + W);
+}
+
+
+// Function for right-angled triangle
+void rightangletriangle(int base, int height, int &Area, int &Per) {
+    Area = (base * height) / 2;
+    
+    int hypotenuse = sqrt(base * base + height * height);
+    Per = base + height + hypotenuse;
 }
 
 
@@ -46,6 +56,8 @@ int main(){
 
               cin >>shape;
 
+
+
               if (shape == "Square" || shape == "square"){
                 int L;
                 cout << "Enter the length of the square: ";
@@ -55,6 +67,8 @@ int main(){
                 cout << "Area of square equals " << area <<endl; 
                 cout << "Perimeter of square equals " << per<<endl <<endl;
               }
+
+
                 else if (shape == "rectangle" || shape == "Rectangle") {
                      int L, W;
                      cout << "Enter the length of the rectangle: ";
@@ -65,6 +79,19 @@ int main(){
                      cout << "-------------------------------------------" << endl;
                      cout << "Area of rectangle equals " << area << endl;
                      cout << "Perimeter of rectangle equals " << per << endl << endl;
+               }
+
+
+                else if (shape == "right-angled triangle" || shape == "Right-angled triangle" || shape == "right angle triangle" || shape == "Right angle triangle" || shape == "right triangle" || shape == "Right triangle") {
+                    int base, height;
+                    cout << "Enter the base of the triangle: ";
+                    cin >> base;
+                    cout << "Enter the height of the triangle: ";
+                    cin >> height;
+                    rightangletriangle(base, height, area, per);
+                    cout << "-------------------------------------------" << endl;
+                    cout << "Area of right-angled triangle equals " << area << endl;
+                    cout << "Perimeter of right-angled triangle equals " << per << endl << endl;
             }
 
         }
