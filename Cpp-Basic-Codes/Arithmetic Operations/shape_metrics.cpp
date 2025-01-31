b55942e3a9a0de8ed0362f4550899ff5a9320253
KareemHassan <kareem99710@gmail.com>
2025-01-31 23:42:38 +0200

    Square Function added

--- Cpp-Basic-Codes/Arithmetic Operations/shape_metrics.cpp
+++ Cpp-Basic-Codes/Arithmetic Operations/shape_metrics.cpp
@@ -7,6 +7,11 @@
 #include <string>
 using namespace std;
 
+void square(int L , int & Area , int & Per){
+    Area = L*L;
+    Per = L*4;
+}
+
 
 int main(){
     cout << "==================================="<<endl;
@@ -15,13 +20,34 @@ int main(){
 
     int choice;
     string shape;
+    int area , per;
     for (;;){
         cout << "Choose 1 to continue or 0 to stop.";
         cin>> choice;
+        cout << "-----------------------------------"<<endl<<endl;
         if (choice == 0 ){
             break;
         }
         else if(choice ==1 ){
+              cout << "=============================================" << endl;
+              cout << " Choose a shape from the following options: " << endl;
+              cout << "-------------------------------------------" << endl;
+              cout << "   1. Square" << endl;
+              cout << "   2. Rectangle" << endl;
+              cout << "   3. Right-Angled Triangle" << endl;
+              cout << "=============================================" << endl;
+
+              cin >>shape;
+
+              if (shape == "Square" || shape == "square"){
+                int L;
+                cout << "Enter the length of the square: ";
+                cin>> L;
+                square(L , area , per);
+                cout << "-------------------------------------------" << endl;
+                cout << "Area of square equals " << area <<endl; 
+                cout << "Perimeter of square equals " << per<<endl <<endl;
+              }
 
         }
         else{
