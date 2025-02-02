8a056d618c293edde2d08795ef5dc453cbd4d3b8
KareemHassan <kareem99710@gmail.com>
2025-02-03 00:13:22 +0200

    Q. Way Too Long Words add comments

--- Cpp-Basic-Codes/CodeForces/Way Too Long Words.cpp
+++ Cpp-Basic-Codes/CodeForces/Way Too Long Words.cpp
@@ -6,14 +6,18 @@ int main() {
     int n;
     cin >> n;
  
+        // Loop through each word
     for (int i = 0; i < n; i++) {
         string word;
-        cin >> word;
- 
+        cin >> word; 
+
+        // If the word length is greater than 10:
         if (word.length() > 10) {
+            // Word: first letter + number of middle letters + last letter
             cout << word.front() << word.length() - 2 << word.back() << "\n";
         }
         else {
+            // If word length is 10 or less, print it as is
             cout << word << endl;
         }
     }
