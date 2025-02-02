bfc6882c00c7c676c243c9906a2c45c50c257ffe
KareemHassan <kareem99710@gmail.com>
2025-02-02 23:45:24 +0200

    Q. Bear and Big Brother add comments

--- Cpp-Basic-Codes/CodeForces/Bear And Big Brother.cpp
+++ Cpp-Basic-Codes/CodeForces/Bear And Big Brother.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 using namespace std;
- 
+
 int main() {
-	int a, b;
-	int years = 0;
-	cin >> a >> b;
-	while (a <= b) {
-		a = a * 3;
-		b = b * 2;
-		years = years + 1;
- 
-	}
-	cout << years;
+    int a, b;
+    int years = 0;
+
+    cin >> a >> b;  
+
+    // multiplying a by 3 and b by 2 until both are equal to each others
+    while (a <= b) { 
+        a = a * 3;
+        b = b * 2; 
+        years++;    
+    }
+
+
+    cout << years;  
 }
