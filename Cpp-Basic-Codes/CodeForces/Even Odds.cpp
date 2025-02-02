95f9704f131536f082a3b1fb9fa831583ccb0132
KareemHassan <kareem99710@gmail.com>
2025-02-02 23:53:40 +0200

    Q. Even Odds add comments

--- Cpp-Basic-Codes/CodeForces/Even Odds.cpp
+++ Cpp-Basic-Codes/CodeForces/Even Odds.cpp
@@ -3,14 +3,16 @@
 using namespace std;
  
 int main() {
-	long long n, k;
+	long long n, k; //used long long to hold bigger numbers as specified in the question
 	cin >> n >> k;
-	long long odd = (n + 1) / 2;
+	long long odd = (n + 1) / 2; // Calculate the number of odd numbers
  
 	if (k <= odd) {
+    // If k is within the odd numbers
 		cout << 2 * k - 1;
 	}
 	else {
+    // If k is within the even numbers
 		cout << 2 * (k - odd);
 	}
 }
