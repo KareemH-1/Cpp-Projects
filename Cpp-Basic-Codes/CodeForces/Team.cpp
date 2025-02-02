b388a3310293478049c3d28df8d3154a1042141d
KareemHassan <kareem99710@gmail.com>
2025-02-03 00:08:57 +0200

    Q. Team add comments

--- Cpp-Basic-Codes/CodeForces/Team.cpp
+++ Cpp-Basic-Codes/CodeForces/Team.cpp
@@ -4,14 +4,16 @@ using namespace std;
 int main() {
     int n, count = 0;
     cin >> n;
- 
+    
+     // Loop through each problem
     for (int i = 0; i < n; i++) {
         int petya, vasya, tonya;
-        cin >> petya >> vasya >> tonya;
+        cin >> petya >> vasya >> tonya; // Read if each person is sure about the solution
  
         
+        // If at least two of them are sure (sum >= 2), they will solve the problem
         if (petya + vasya + tonya >= 2) {
-            count++;
+            count++; // Increase the count of problems they solve +1
         }
     }
  
