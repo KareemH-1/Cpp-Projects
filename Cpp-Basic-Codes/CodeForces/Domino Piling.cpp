7d1732d5daaee2f4f87459de828456c17b5a6c64
KareemHassan <kareem99710@gmail.com>
2025-02-03 00:22:29 +0200

    Q. Domino Piling add comments

--- Cpp-Basic-Codes/CodeForces/Domino Piling.cpp
+++ Cpp-Basic-Codes/CodeForces/Domino Piling.cpp
@@ -4,11 +4,15 @@ using namespace std;
 int main() {
 	int M, N , area;
 	cin >> M >> N;
-	area = M * N;
-	int dominos;
-	if (area % 2 != 0) {
-		area = area-1;
-	}
-	dominos = area / 2;
+	area = M * N;  // Calculate the total number of squares on the board
+    int dominos; 
+
+    // If the area is odd, reduce it by 1 because we can't fit a domino in the last square
+    if (area % 2 != 0) {
+        area = area - 1;
+    }
+
+    // Each domino covers 2 squares, so the maximum number of dominoes is half the area
+    dominos = area / 2;
 	cout << dominos;
 }
