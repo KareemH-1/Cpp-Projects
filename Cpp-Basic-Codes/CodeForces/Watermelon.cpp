23a6eb4c19df0eb8735181cd5dc69976a6af10c5
KareemHassan <kareem99710@gmail.com>
2025-02-03 00:03:30 +0200

    Q. Watermelon add comments

--- Cpp-Basic-Codes/CodeForces/Watermelon.cpp
+++ Cpp-Basic-Codes/CodeForces/Watermelon.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int main() {
     int w;
     cin >> w;
+    
+    // Check if the weight is greater than 2 and is an even number
     if (w > 2 && w % 2 == 0) {
-        cout << "YES" << endl;
+        cout << "YES" << endl; // If both conditions are met, they can split the watermelon
     }
     else {
-        cout << "NO" << endl;
+        cout << "NO" << endl; // Otherwise, they can't split it as required
     }
-}
