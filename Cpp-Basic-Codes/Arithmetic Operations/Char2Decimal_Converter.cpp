6b9fac6ff014ce8d58d0f823979d97376fea7d31
KareemHassan <kareem99710@gmail.com>
2025-02-02 20:51:38 +0200

    Add ASCII converter of a char to to int code

---
+++ Cpp-Basic-Codes/Arithmetic Operations/Char2Decimal_Converter.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char X; // input
+    cout << "Please enter the character you want to convert: ";
+    cin>> X;
+
+    cout << "The Decimal representation of "<< X << "In ASCII table is: "<<int(X); //converting the char variable to int
+}
