b41bdcabc4c088be17650b818a24145317afd3cc
KareemHassan <kareem99710@gmail.com>
2025-02-10 01:02:17 +0200

    Add ascii art for displaying the dice

--- Cpp-Basic-Codes/Games/diceSimulator.cpp
+++ Cpp-Basic-Codes/Games/diceSimulator.cpp
@@ -8,23 +8,66 @@ int rollDie() {
     return (rand() % 6) + 1; // Generates a number between 1 and 6
 }
 
+// Function to display dice faces
+void printDice(int value) {
+    if (value == 1) {
+        cout << " ------- "<<endl;
+        cout << "|       |"<<endl;
+        cout << "|   O   |"<<endl;
+        cout << "|       |"<<endl;
+        cout << " ------- "<<endl;
+    } 
+    else if (value == 2) {
+        cout << " ------- "<<endl;
+        cout << "| O     |"<<endl;
+        cout << "|       |"<<endl;
+        cout << "|     O |"<<endl;
+        cout << " ------- "<<endl;
+    } 
+    else if (value == 3) {
+        cout << " ------- "<<endl;
+        cout << "| O     |"<<endl;
+        cout << "|   O   |"<<endl;
+        cout << "|     O |"<<endl;
+        cout << " ------- "<<endl;
+    } 
+    else if (value == 4) {
+        cout << " ------- "<<endl;
+        cout << "| O   O |"<<endl;
+        cout << "|       |"<<endl;
+        cout << "| O   O |"<<endl;
+        cout << " ------- "<<endl;
+    } 
+    else if (value == 5) {
+        cout << " ------- "<<endl;
+        cout << "| O   O |"<<endl;
+        cout << "|   O   |"<<endl;
+        cout << "| O   O |"<<endl;
+        cout << " ------- "<<endl;
+    } 
+    else if (value == 6) {
+        cout << " ------- "<<endl;
+        cout << "| O   O |"<<endl;
+        cout << "| O   O |"<<endl;
+        cout << "| O   O |"<<endl;
+        cout << " ------- "<<endl;
+    }
+}
+
+
 int main() {
-    srand(time(0)); // Seed random number generator with current time
+    srand(time(0)); 
 
-    int numRolls;
-    cout << "Welcome to the Dice Simulator!"<<endl;
-    cout << "How many times would you like to roll the dice? ";
-    cin >> numRolls;
+    char choice;
+    cout << "Welcome to the Dice Simulator!" <<endl;
+    while (choice == 'y' || choice == 'Y'|| choice == '1') {
+        int roll = rollDie();
+        cout << "You rolled: " << roll << endl;
+        printDice(roll);
 
-    if (numRolls <= 0) {
-        cout << "Invalid number of rolls! Exiting..."<<endl;
-        return 1;
+        cout << "Do you want to roll again? (Y/N): ";
+        cin >> choice;
     }
 
-    cout << "Rolling the dice..."<<endl;
-    for (int i = 0; i < numRolls; i++) {
-        cout << "Roll " << (i + 1) << ": " << rollDie() << endl;
-    }
-
-    cout << "GoodBye!";
+    cout << "Goodbye!";
 }
