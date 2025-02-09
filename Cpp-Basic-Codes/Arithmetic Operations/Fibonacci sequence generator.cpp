71dd829cbb733bdbe69316895ea947a29356e47e
KareemHassan <kareem99710@gmail.com>
2025-02-09 20:36:47 +0200

    add more conditions

--- Cpp-Basic-Codes/Arithmetic Operations/Fibonacci sequence generator.cpp
+++ Cpp-Basic-Codes/Arithmetic Operations/Fibonacci sequence generator.cpp
@@ -1,12 +1,25 @@
-//Generate Fibonacci numbers up to an input number.
 #include <iostream>
 using namespace std;
+
 int main() {
     int n;
     cout << "Enter the number of Fibonacci numbers to generate: ";
     cin >> n;
+
+    if (n <= 0) {
+        cout << "Please enter a positive number." << endl;
+        return 0; //exit code if user enters a negative number or zero
+    }
+
     int a = 0, b = 1;
+    
+     if (n == 1) {
+        cout << "Fibonacci sequence: " << a << endl;
+        return 0; //exits code if user enters 1 after displaying the first number in sequence (0)
+    }
+
     cout << "Fibonacci sequence: " << a << " " << b;
+    
     for (int i = 2; i < n; i++) {
         int next = a + b;
         cout << " " << next;
