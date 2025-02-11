bcc8b370d6f26d5190bc3c768bfec2143ea9c90b
KareemHassan <kareem99710@gmail.com>
2025-02-11 22:12:58 +0200

    Add more encryption & decryption for both lowercase and uppercase letters

--- Cpp-Basic-Codes/Char Arrays & Strings/basic Encryption_Decryption system.cpp
+++ Cpp-Basic-Codes/Char Arrays & Strings/basic Encryption_Decryption system.cpp
@@ -1,122 +1,132 @@
 #include <iostream>
 using namespace std;
-void encryption(char x[]) {
-	for (int i = 0; x[i] != '\0'; i++) {
-		if (x[i] == 'a') {
+
+void encryption(char x[]) 
+{
+	for (int i = 0; x[i] != '\0'; i++) 
+	{
+		if (x[i] == 'a') 
+		{
 			x[i] = '#';
 		}
-		else if (x[i] == 'A') {
+		else if (x[i] == 'A') 
+		{
 			x[i] = '@';
 		}
-		else if (x[i] == 'e') {
+		else if (x[i] == 'e') 
+		{
 			x[i] = '3';
 		}
-		else if (x[i] == 'E') {
+		else if (x[i] == 'E') 
+		{
 			x[i] = '8';
 		}
-		else if (x[i] == 'i') {
+		else if (x[i] == 'i') 
+		{
 			x[i] = '1';
 		}
-		else if (x[i] == 'I') {
+		else if (x[i] == 'I') 
+		{
 			x[i] = '!';
 		}
-		else if (x[i] == 'o') {
+		else if (x[i] == 'o') 
+		{
 			x[i] = '0';
 		}
-		else if (x[i] == 'O') {
+		else if (x[i] == 'O') 
+		{
 			x[i] = '*';
 		}
-		else if (x[i] == 's') {
+		else if (x[i] == 's') 
+		{
 			x[i] = '5';
 		}
-		else if (x[i] == 'S') {
+		else if (x[i] == 'S') 
+		{
 			x[i] = '$';
 		}
-		else if (x[i] == 't') {
-			x[i] = '+'; 
+		else if (x[i] == 'g') 
+		{
+			x[i] = '&';
 		}
-		else if (x[i] == 'T') {
-			x[i] = '^'; 
+		else if (x[i] == 'B') 
+		{
+			x[i] = '%';
 		}
-		else if (x[i] == 'g') {
-			x[i] = '&'; 
+		else if (x[i] == 'l') 
+		{
+			x[i] = '|';
 		}
-		else if (x[i] == 'G') {
-			x[i] = '9'; 
-		}
-		else if (x[i] == 'b') {
-			x[i] = '6'; 
-		}
-		else if (x[i] == 'B') {
-			x[i] = '%'; 
-		}
-		else if (x[i] == 'c') {
-			x[i] = '('; 
-		}
-		else if (x[i] == 'C') {
-			x[i] = ')'; 
+		else if (x[i] == 't') 
+		{
+			x[i] = '^';
 		}
 	}
 }
 
-void decryption(char x[]) {
-	for (int i = 0; x[i] != '\0'; i++) {
-		if (x[i] == '#') {
+void decryption(char x[]) 
+{
+	for (int i = 0; x[i] != '\0'; i++) 
+	{
+		if (x[i] == '#') 
+		{
 			x[i] = 'a';
 		}
-		else if (x[i] == '@') {
+		else if (x[i] == '@') 
+		{
 			x[i] = 'A';
 		}
-		else if (x[i] == '3') {
+		else if (x[i] == '3') 
+		{
 			x[i] = 'e';
 		}
-		else if (x[i] == '8') {
+		else if (x[i] == '8') 
+		{
 			x[i] = 'E';
 		}
-		else if (x[i] == '1') {
+		else if (x[i] == '1') 
+		{
 			x[i] = 'i';
 		}
-		else if (x[i] == '!') {
+		else if (x[i] == '!') 
+		{
 			x[i] = 'I';
 		}
-		else if (x[i] == '0') {
+		else if (x[i] == '0') 
+		{
 			x[i] = 'o';
 		}
-		else if (x[i] == '*') {
+		else if (x[i] == '*') 
+		{
 			x[i] = 'O';
 		}
-		else if (x[i] == '5') {
+		else if (x[i] == '5') 
+		{
 			x[i] = 's';
 		}
-		else if (x[i] == '$') {
+		else if (x[i] == '$') 
+		{
 			x[i] = 'S';
 		}
-		else if (x[i] == '+') {
-			x[i] = 't';
-		}
-		else if (x[i] == '^') {
-			x[i] = 'T';
-		}
-		else if (x[i] == '&') {
+		else if (x[i] == '&') 
+		{
 			x[i] = 'g';
 		}
-		else if (x[i] == '9') {
-			x[i] = 'G';
-		}
-		else if (x[i] == '6') {
-			x[i] = 'b';
-		}
-		else if (x[i] == '%') {
+		else if (x[i] == '%') 
+		{
 			x[i] = 'B';
 		}
-		else if (x[i] == '(') {
-			x[i] = 'c';
+		else if (x[i] == '|') 
+		{
+			x[i] = 'l';
 		}
-		else if (x[i] == ')') {
-			x[i] = 'C';
+		else if (x[i] == '^') 
+		{
+			x[i] = 't';
 		}
 	}
 }
+
 
 
 int main() {
@@ -129,7 +139,7 @@ int main() {
 		cin >> ws;
 		gets_s(x);
 		encryption(x);
-		cout << "Here is the encrypted sentence: " << x;
+		cout << "Here is the encrypted sentence: \n" << x <<endl;
 	}
 	else if (choice == 2) {
 		char x[500];
@@ -137,7 +147,7 @@ int main() {
 		cin >> ws;
 		gets_s(x);
 		decryption(x);
-		cout << "Here is the decrypted sentence: " << x;
+		cout << "Here is the decrypted sentence: \n" << x <<endl;
 	}
 	else {
 		cout << "Invalid choice! try again" <<endl;
