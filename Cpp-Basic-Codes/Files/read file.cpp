99076a3c3d512605049a4beeafb649ca914ab1e9
KareemHassan <kareem99710@gmail.com>
2025-02-15 00:04:00 +0200

    Refactor file reading to output all lines instead of just the first

--- Cpp-Basic-Codes/Files/read file.cpp
+++ Cpp-Basic-Codes/Files/read file.cpp
@@ -10,8 +10,10 @@ int main(){
     file.open(path);
     if (file.is_open()) {
         cout << "File successfully opened." << endl;
-        getline(file, read);
-        cout << read;
+        //read all the lines in the text file
+        while (getline(file, read)) { //while getline returns true (there is a line to read) it will print the line
+            cout << read;
+          }
     }
     else if (!file.is_open()) {
         cout << "File failed to open." << endl;
