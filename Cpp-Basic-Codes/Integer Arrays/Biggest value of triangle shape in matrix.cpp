eaa094e0b8b18595871ade6a21af0220a0ff1e01
KareemHassan <kareem99710@gmail.com>
2025-02-19 17:40:20 +0200

    Add program to calculate the maximum triangle sum in a matrix

---
+++ Cpp-Basic-Codes/Integer Arrays/Biggest value of triangle shape in matrix.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matrix[20][20];
+    for (int i = 0; i < 20; i++) {
+        for (int j = 0; j < 20; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    int maxSum = 0;
+    int bestRow = -1, bestCol = -1, bestSize = -1;
+
+
+    for (int sizeBase = 3; sizeBase <= 19; sizeBase += 2) {
+
+        int height = (sizeBase - 1) / 2;  
+        for (int i = 0; i + height < 20; i++) { 
+            for (int j = 0; j + sizeBase <= 20; j++) {
+
+                int sum = 0;
+                int center = j + sizeBase / 2;
+
+                for (int q = 0; q <= height; q++) {
+                    int left = center - q;
+                    int right = center + q;
+
+
+                    if (left >= 0 && right < 20) {
+                        for (int col = left; col <= right; col++) {
+                            sum += matrix[i + q][col];
+                        }
+                    }
+                }
+
+
+                if (sum > maxSum) {
+                    maxSum = sum;
+                    bestRow = i;
+                    bestCol = j;
+                    bestSize = sizeBase;
+                }
+            }
+        }
+    }
+
+    cout << "Max Triangle Sum: " << maxSum << endl;
+    cout << "Top-left corner of base: (" << bestRow << ", " << bestCol << ")" << endl;
+    cout << "Base size: " << bestSize << endl;
+}
