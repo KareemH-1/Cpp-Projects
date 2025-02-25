dc86d96adcd3ca8e07599fe75e07e028a60e9ea3
KareemHassan <kareem99710@gmail.com>
2025-02-25 18:04:35 +0200

    Add a simple timer application with start and quit functionality

---
+++ Cpp-Basic-Codes/miscellaneous/Timer.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+using namespace std;
+int main() {
+  
+        cout << "==========================" << endl;
+        cout << "           Timer          " << endl;
+		cout << "==========================" << endl;
+        cout << "Enter (Q to Quit) or (S to start)" << endl;
+        char choice;
+		cin >> choice;
+		if (choice == 'Q' || choice == 'q') {
+			cout << "Quitting..." << endl;
+			return 0;
+		
+		}
+		else if (choice == 'S' || choice == 's') {
+			cout << "Starting..." << endl;
+			this_thread::sleep_for(chrono::seconds(1));
+			system("cls");
+
+			int seconds = 0, minutes = 0, hours = 0;
+			int dispSeconds = 0;
+			for (;;) {
+				
+
+				cout << hours << " : " << minutes << " : " << dispSeconds << endl;
+				this_thread::sleep_for(chrono::seconds(1));
+				seconds++;
+				dispSeconds++;
+				if (seconds % 60 == 0) {
+					minutes++;
+					dispSeconds = 0;
+				}
+				if (minutes % 60 == 0 && minutes !=0) {
+					hours++;
+					minutes = 0;
+				}
+				
+				system("cls");
+			}
+		}
+		
+		else {
+			cout << "Invalid input!" << endl;
+			return 0;
+		}
+}
