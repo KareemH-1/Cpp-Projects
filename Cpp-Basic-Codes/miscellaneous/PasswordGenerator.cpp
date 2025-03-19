a4cef7565787468dd9c672429633a88295ef1b46
KareemHassan <kareem99710@gmail.com>
2025-03-20 01:01:39 +0200

    enhance password generator to include customizable character sets and error handling

--- Cpp-Basic-Codes/miscellaneous/PasswordGenerator.cpp
+++ Cpp-Basic-Codes/miscellaneous/PasswordGenerator.cpp
@@ -1,28 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
+
+int main() {
     srand(time(0));
     int length;
     cout << "Enter the length of the password: ";
-    cin>> length;
+    cin >> length;
 
-    char *password = new char[length];
-    char symbols[]= {'!','@','#','$','%','&','*'};
-    for(int i=0; i<length; i++){
-        int numOrCharOrSymbol = rand() % 3;
-        if(numOrCharOrSymbol == 0){
-            password[i] = char(rand() % 26 + 'a');
-        }
-        else if(numOrCharOrSymbol == 1){
-            password[i] = char(rand() % 10 + '0');
-        }
-        else{
-            password[i] = symbols[rand() % 7];
-        }
+    bool includeUppercase, includeLowercase, includeNumbers, includeSymbols;
+    cout << "Include uppercase letters? (1 for yes, 0 for no): ";
+    cin >> includeUppercase;
+    cout << "Include lowercase letters? (1 for yes, 0 for no): ";
+    cin >> includeLowercase;
+    cout << "Include numbers? (1 for yes, 0 for no): ";
+    cin >> includeNumbers;
+    cout << "Include symbols? (1 for yes, 0 for no): ";
+    cin >> includeSymbols;
+
+    if (!includeUppercase && !includeLowercase && !includeNumbers && !includeSymbols) {
+        cout << "Error: You must include at least one character set!" << endl;
+        return 1;
     }
-    cout << "The generated password is: ";
-    for(int i=0; i<length; i++){
-        cout << password[i];
+
+    char uppercase[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char lowercase[] = "abcdefghijklmnopqrstuvwxyz";
+    char numbers[] = "0123456789";
+    char symbols[] = "!@#$%^&*()_+-=:,.<>?";
+
+    string possibilities = "";
+    if (includeUppercase) possibilities += uppercase;
+    if (includeLowercase) possibilities += lowercase;
+    if (includeNumbers) possibilities += numbers;
+    if (includeSymbols) possibilities += symbols;
+
+    char* password = new char[length + 1];
+
+    for (int i = 0; i < length; i++) {
+        int randomIndex = rand() % possibilities.length();
+        password[i] = possibilities[randomIndex];
     }
-    delete[]password;
+
+    password[length] = '\0';
+
+    cout << "The generated password is: " << password << endl;
+
+    delete[] password;
 }
