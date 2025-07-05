b992c8006727d3b05bb0015faf9b2f9dd291ad10
KareemHassan <kareem99710@gmail.com>
2025-07-05 18:19:29 +0300

    Add binary search tree

--- Cpp-Basic-Codes/DataStrucutre/DatastructureClasses.cpp
+++ Cpp-Basic-Codes/DataStrucutre/DatastructureClasses.cpp
@@ -621,4 +621,84 @@ namespace DLL{
 
         };
     }
+    namespace BST{
+
+        class Node { 
+            public: 
+                int value;
+                Node* left;
+                Node* right;
+        
+                Node(int value) {
+                    this->value = value;
+                    left = nullptr;
+                    right = nullptr;
+                }
+        };
+
+
+        class BinarySearchTree {
+            private:
+                Node* root;
+        
+            public:
+                BinarySearchTree() { root = nullptr; }
+        
+                void destroy(Node* currentNode) {
+                    if (currentNode == nullptr) return;
+                    if (currentNode->left) destroy(currentNode->left);
+                    if (currentNode->right) destroy(currentNode->right);
+                    delete currentNode;
+                }
+            
+                ~BinarySearchTree() { destroy(root); }
+            
+                Node* getRoot() {
+                    return root;
+                } 
+            
+                bool insert(int value) {
+                    Node* newNode = new Node(value);
+                    if (root == nullptr) {
+                        root = newNode;
+                        return true;
+                    }
+                    Node* temp = root;
+                    while(true) {
+                        if (newNode->value == temp->value) return false;
+                        if (newNode->value < temp->value) {
+                            if (temp->left == nullptr) {
+                                temp->left = newNode;
+                                return true;
+                            }
+                            temp = temp->left;
+                        } else {
+                            if (temp->right == nullptr) {
+                                temp->right = newNode;
+                                return true;
+                            }
+                            temp = temp->right;
+                        }
+                    }
+                }
+
+                bool contains(int val){
+                    if(root == nullptr) return false;
+                    Node * temp = root;
+                    while(temp){
+                        if(val < temp->value){
+                            temp= temp-> left;
+                        }
+                        else if(val > temp->value){
+                            temp= temp->right;
+                        }
+                        else return true;
+                    }
+                    return false;
+                }       
+
+        };
+
+
+    }
 }
